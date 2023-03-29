8ef000e6bbe630abe1f1659563d9e2500563908f
LeM <mihail.lebedev88@gmail.com>
2023-03-29 12:41:57 +0600

    Initial commit

---
+++ src/main/java/ru/netology/javaqa/javaqamvnhw/services/StatsService.java
@@ -0,0 +1,64 @@
+package ru.netology.javaqa.javaqamvnhw.services;
+
+public class StatsService {
+
+    public double calculateTotalSales(double[] sales) {
+        int sum = 0;
+        for (double sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    public double calculateAverageSales(double[] sales) {
+        double totalSales = calculateTotalSales(sales);
+        return totalSales / sales.length;
+    }
+
+    public int findMaxSalesMonth(double[] sales) {
+        int maxMonth = 0;
+        double maxSales = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] > maxSales) {
+                maxSales = sales[i];
+                maxMonth = i;
+            }
+        }
+        return maxMonth;
+    }
+
+    public int findMinSalesMonth(double[] sales) {
+        int minMonth = 0;
+        double minSales = sales[0];
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < minSales) {
+                minSales = sales[i];
+                minMonth = i;
+            }
+        }
+        return minMonth;
+    }
+
+    public int CountBelowAverageSales(double[] sales) {
+        double averageSales = calculateAverageSales(sales);
+        int count = 0;
+        for (double sale : sales) {
+            if (sale < averageSales) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public int CountAboveAverageSales(double[] sales) {
+        double averageSales = calculateAverageSales(sales);
+        int count = 0;
+        for (double sale : sales) {
+            if (sale > averageSales) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+
---
+++ src/test/java/ru/netology/javaqa/javaqamvnhw/services/StatsServiceTest.java
@@ -0,0 +1,77 @@
+package ru.netology.javaqa.javaqamvnhw.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class StatsServiceTest {
+
+    @Test
+    public void shouldFindTotalSales() {
+        StatsService service = new StatsService();
+
+        double[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        double expected = 180;
+        double totalSales = service.calculateTotalSales(sales);
+
+        Assertions.assertEquals(expected, totalSales);
+    }
+
+    @Test
+    public void shouldFindAverageSales() {
+        StatsService service = new StatsService();
+
+        double[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        double expected = 15;
+        double averageSales = service.calculateAverageSales(sales);
+
+        Assertions.assertEquals(expected, averageSales);
+    }
+    @Test
+    public void shouldFindMaxSalesMonth() {
+        StatsService service = new StatsService();
+
+        double[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expected = 5;
+        int maxSales = service.findMaxSalesMonth(sales);
+
+        Assertions.assertEquals(expected, maxSales);
+    }
+    @Test
+    public void shouldFindMinSalesMonth() {
+        StatsService service = new StatsService();
+
+        double[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expected = 8;
+        int minSales = service.findMinSalesMonth(sales);
+
+        Assertions.assertEquals(expected, minSales);
+    }
+    @Test
+    public void shouldFindCountMonthBelowAverage() {
+        StatsService service = new StatsService();
+
+        double[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expected = 5;
+        int actual = service.CountBelowAverageSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldFindCountMonthAboveAverage() {
+        StatsService service = new StatsService();
+
+        double[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expected = 5;
+        int actual = service.CountAboveAverageSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
+
